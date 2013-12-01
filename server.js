2391814f4b8e9143bca4f84cdef4a51e4fb6951e
akbarahmed <akbar501@gmail.com>
2013-12-01 04:53:01 -0800

    support multi routes files; moves routes to sever dir

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 /**
- * Module dependencies.
+ * @file Express server startup file
  */
+
 var express  = require('express'),
     fs       = require('fs'),
     passport = require('passport'),
     logger   = require('mean-logger')
     colors   = require('colors');
 
-/**
- * Main application entry file.
- * Please note that the order of loading is important.
- */
+// Important: The order of loading is important
 
 // Load configurations
 // if test env, load example file
@@ -19,13 +17,20 @@ var env      = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     auth     = require('./config/middlewares/authorization'),
     mongoose = require('mongoose');
 
-// Bootstrap db connection
+// Project directories
+var dirRoot         = __dirname,
+    dirServer       = dirRoot + '/server',
+    dirServerModels = dirServer + '/models',
+    dirServerRoutes = dirServer + '/routers',
+    dirClient       = dirRoot + '/client';
+
+// Connect to MongoDB
 mongoose.connection.on("open", function(ref) {
-  return console.log('Exponential: ' + 'Connected to MongoDB'.green);
+  return console.log('Exponential: '.blue + 'Connected to MongoDB'.green);
 });
 
 mongoose.connection.on("error", function(err) {
-  console.log('Exponential: ' + 'Failed to connect to MongoDB'.red);
+  console.log('Exponential: '.blue + 'Failed to connect to MongoDB'.red);
   return console.log(err.message.red);
 });
 
@@ -36,7 +41,7 @@ try {
   console.log('Exponential: ' + err.message.red);
 }
 
-// Load DB models
+// Load Mongoose models
 var models_path = __dirname + '/app/models';
 var walk = function(path) {
     fs.readdirSync(path).forEach(function(file) {
@@ -61,15 +66,29 @@ var app = express();
 // express settings
 require('./config/express')(app, passport, db);
 
-// Bootstrap routes
-require('./config/routes')(app, passport, auth);
+// Load all Express routes
+var loadRoutes = function(path) {
+  fs.readdirSync(path).forEach(function(file) {
+    var routerPath = path + '/' + file;
+    var stat = fs.statSync(routerPath);
+      if (stat.isFile()) {
+        if (/(.*)\.(js$)/.test(file)) {
+          console.log('Exponential: '.blue + 'Loading router '.green + routerPath.green);
+          require(routerPath)(app, passport, auth);
+        }
+      } else if (stat.isDirectory()) {
+        loadRoutes(routerPath);
+      }
+    });
+};
+loadRoutes(dirServerRoutes);
 
 // Start the app by listening on <port>
 var port = process.env.PORT || config.port;
 app.listen(port);
 
 var startMessage = 'App started on port ' + port;
-console.log('Exponential: ' + startMessage.green);
+console.log('Exponential: '.blue + startMessage.green);
 
 // Initializing logger
 logger.init(app, passport, mongoose);
